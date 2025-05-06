62e85c39a3222590e8526d932916b71f9b45a300
sivanargana <siva.nargana66@gmail.com>
2025-05-07 00:11:19 +0530

    init

---
+++ app.py
@@ -0,0 +1,118 @@
+from flask import Flask,render_template,redirect,url_for
+import os
+app = Flask(__name__,template_folder="src/views",static_folder='src/assets')
+
+import data
+ 
+
+@app.route('/')
+def home():
+    return redirect(url_for('index'))
+
+@app.route('/index.html')
+def index():
+    return render_template("index.html",data=data.data["courses"])
+
+@app.route('/progress.html')
+def progress():
+
+   pages =  data.data["pages"]
+   html = sum(1 for page in pages if page["html"] == "completed")
+   react = sum(1 for page in pages if page["react"] == "completed")
+   length = len(pages)
+ 
+
+   return render_template("/progress.html",data={"pages":pages,"html":html,"react":react,"length":length})
+
+@app.route('/about-us.html')
+def aboutus():
+    return render_template("about-us.html")
+
+@app.route('/login.html')
+def login():
+    return render_template("/login.html")
+
+@app.route('/register.html')
+def register():
+    return render_template("/register.html")
+
+@app.route('/forgot-password.html')
+def forgotpassword():
+    return render_template("/forgot-password.html")
+
+@app.route('/dashboard.html')
+def dashboard():
+    return render_template("/dashboard.html")
+
+@app.route('/attendance.html')
+def attendance():
+    return render_template("/attendance.html")
+
+@app.route('/certifications.html')
+def certifications():
+    return render_template("/certifications.html")
+
+@app.route('/certifications-empty.html')
+def certificationsempty():
+    return render_template("/certifications-empty.html")
+
+@app.route('/upload-question.html')
+def uploadquestion():
+    return render_template("/upload-question.html")
+
+@app.route('/profile.html')
+def profile():
+    return render_template("/profile.html")
+
+@app.route('/profile-step2.html')
+def profilestep2():
+    return render_template("/profile-step2.html")
+
+@app.route('/profile-step3.html')
+def profilestep3():
+    return render_template("/profile-step3.html")
+
+@app.route('/profile-step4.html')
+def profilestep4():
+    return render_template("/profile-step4.html")
+
+@app.route('/change-password.html')
+def changepassword():
+    return render_template("/change-password.html")
+
+@app.route('/live-sessions.html')
+def livesessions():
+    return render_template("/live-sessions.html")
+
+@app.route('/courses.html')
+def courses():
+    return render_template("/courses.html")
+
+@app.route('/download-pdfs.html')
+def downloadpdfs():
+    return render_template("/download-pdfs.html")
+
+@app.route('/daily-puzzles.html')
+def dailypuzzles():
+    return render_template("/daily-puzzles.html")
+
+@app.route('/practice-assigned-tests.html')
+def practiceassignedtests():
+    return render_template("/practice-assigned-tests.html",data=data.data["tests"])
+
+@app.route('/practice-completed-tests.html')
+def practicecompletedtests():
+    return render_template("/practice-completed-tests.html",data=data.data["tests"])
+
+@app.route('/series-assigned-tests.html')
+def seriesassignedtests():
+    return render_template("/series-assigned-tests.html",data=data.data["tests"])
+
+@app.route('/series-completed-tests.html')
+def seriescompletedtests():
+    return render_template("/series-completed-tests.html",data=data.data["tests"])
+
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
---
+++ data.py
@@ -0,0 +1,131 @@
+data = {
+    "courses":[
+        {
+            "title":"Campus Recruitment Training",
+            "content":"This Course will be helpful for Students who are going to appear for any Job Placement/Interview like Infosys, TCS NQT, CapGemini, Accenture, Oracle and many more",
+            "price":"3500.00",
+            "image":"campus_recruiting.jpg"
+        },
+        {
+            "title":"Core Java",
+            "content":"Learning Java can be a great skill to acquire if you want to work in the programming or software",
+            "price":"4000.00",
+            "image":"core_java.jpg"
+        }
+        ,{
+            "title":"Campus Recruitment Training (Recorded)",
+            "content":"This Course will be helpful for Students who are going to appear for any Job Placement/Interview like",
+            "price":"3000.00",
+            "image":"carmpus_recruitment_core.jpg"
+        }
+        ,{
+            "title":"CRT + Core Java (Combo)",
+            "content":"This Course will be helpful for Students who are going to appear for any Job Placement/Interview like",
+            "price":"5000.00",
+            "image":"crt_java.jpg"
+        }
+        
+        ,{
+            "title":"GATE & ESE (Aptitude)",
+            "content":"In this Course, we cover the entire 15 marks of GATE Aptitude, covering Logical Reasoning, Quantitative",
+            "price":"3000.00",
+            "image":"gate_ese.jpg"
+        }
+        ,{
+            "title":"Python (Core & Advance)",
+            "content":"Python is general-purpose, high-level, interpreted, interactive, object-oriented, and open-source. It is",
+            "price":"7000.00",
+            "image":"python_course.jpg"
+        }
+        
+        
+        
+       
+        
+    ],
+    "tests":[
+    {
+        "category": "Logical Reasoning",
+        "topic": "Letter Series",
+        "test_name": "Practice Test - 1",
+        "no_of_questions": 5,
+        "duration": 5,
+        "image": "e2.jpeg"
+    },
+    {
+        "category": "Arithmetic",
+        "topic": "Problems on Ages",
+        "test_name": "Problems on Ages Test 1",
+        "no_of_questions": 10,
+        "duration": 15,
+        "image": "e1.jpeg"
+    },
+    {
+        "category": "Arithmetic",
+        "topic": "Permutation & Combination",
+        "test_name": "P & C Test 1",
+        "no_of_questions": 10,
+        "duration": 15,
+        "image": "e3.jpeg"
+    },
+    {
+        "category": "Logical Reasoning",
+        "topic": "Coding & Decoding",
+        "test_name": "Coding & Decoding Test 1",
+        "no_of_questions": 5,
+        "duration": 10,
+        "image": "e2.jpeg"
+    },
+    {
+        "category": "Arithmetic",
+        "topic": "Averages & Mixtures",
+        "test_name": "Averages & Mixtures Test 1",
+        "no_of_questions": 10,
+        "duration": 10,
+        "image": "e1.jpeg"
+    },
+    {
+        "category": "Arithmetic",
+        "topic": "Ratio & Proportions",
+        "test_name": "Ratio & Proportion - Test 1",
+        "no_of_questions": 5,
+        "duration": 10,
+        "image": "e3.jpeg"
+    },
+    {
+        "category": "Logical Reasoning",
+        "topic": "CryptArithmetic",
+        "test_name": "Cryptarithmetic Test 1",
+        "no_of_questions": 3,
+        "duration": 10,
+        "image": "e1.jpeg"
+    }
+],
+"pages": [
+  {"page": "About Us", "link": "about-us.html", "html": "completed", "react": "completed", "type": "Standard"},
+  {"page": "Attendance", "link": "attendance.html", "html": "completed", "react": "completed", "type": "Standard"},
+  {"page": "Certifications (Empty)", "link": "certifications-empty.html", "html": "completed", "react": "completed", "type": "Standard"},
+  {"page": "Certifications", "link": "certifications.html", "html": "completed", "react": "completed", "type": "Standard"},
+  {"page": "Change Password", "link": "change-password.html", "html": "pending", "react": "pending", "type": "Standard"},
+  {"page": "Courses", "link": "courses.html", "html": "pending", "react": "pending", "type": "Standard"},
+  {"page": "Daily Puzzles", "link": "daily-puzzles.html", "html": "pending", "react": "pending", "type": "Standard"},
+  {"page": "Dashboard", "link": "dashboard.html", "html": "partial", "react": "partial", "type": "Standard"},
+  {"page": "Download PDFs", "link": "download-pdfs.html", "html": "pending", "react": "pending", "type": "Standard"},
+  {"page": "Forgot Password", "link": "forgot-password.html", "html": "completed", "react": "completed", "type": "Standard"},
+  {"page": "Home", "link": "index.html", "html": "completed", "react": "completed", "type": "Advanced"},
+  {"page": "Live Sessions", "link": "live-sessions.html", "html": "pending", "react": "pending", "type": "Standard"},
+  {"page": "Login", "link": "login.html", "html": "completed", "react": "completed", "type": "Standard"},
+  {"page": "Practice Assigned Tests", "link": "practice-assigned-tests.html", "html": "completed", "react": "completed", "type": "Standard"},
+  {"page": "Practice Completed Tests", "link": "practice-completed-tests.html", "html": "completed", "react": "completed", "type": "Standard"},
+  {"page": "Profile", "link": "profile.html", "html": "completed", "react": "completed", "type": "Standard"},
+  {"page": "Register", "link": "register.html", "html": "completed", "react": "completed", "type": "Standard"},
+  {"page": "Series Assigned Tests", "link": "series-assigned-tests.html", "html": "completed", "react": "completed", "type": "Standard"},
+  {"page": "Series Completed Tests", "link": "series-completed-tests.html", "html": "completed", "react": "completed", "type": "Standard"},
+  {"page": "Upload Question", "link": "upload-question.html", "html": "pending", "react": "pending", "type": "Standard"}
+]
+
+
+}
+
+
+ 
